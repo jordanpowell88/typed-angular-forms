b135c6c69975391de18fd8d01ae157e18f7f0741
jordan powell <jordan.powell@siriusxm.com>
2021-04-11 20:45:11 -0400

    initial commit

---
+++ apps/briebug-forms/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'briebug-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'briebug-forms';
+}
